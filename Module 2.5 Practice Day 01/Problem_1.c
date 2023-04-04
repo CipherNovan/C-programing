f378acec9e9cbb3d4484085d5cdcd8b71d155102
CipherNovan <129555025+CipherNovan@users.noreply.github.com>
2023-04-04 17:56:07 +0600

    Add files via upload

---
+++ Module 2.5 Practice Day 01/Problem_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam.\n");
+    printf("This is brilliant!\n");
+    printf("I wish you’ll shine in your life!\t\tGood luck with all the barriers(/\\) in your life.");
+    return 0;
+}
+// printf er vitore double cotetion" " vitore new line die sob likha jete pare 
+//or printf die alada alda vave difrent line likha jete pare   
+//like this:
+#include <stdio.h>
+int main ()
+{
+
+    printf("Recently I heard that you've achieved 95%% marks in your exam.\nThis is brilliant!\n");
+    printf("I wish you'll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
+    return 0;
+}
