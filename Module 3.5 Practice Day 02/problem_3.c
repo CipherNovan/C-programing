fe442c9a2a6d134537dd09ca15a29188df9ae99d
CipherNovan <129555025+CipherNovan@users.noreply.github.com>
2023-04-06 18:17:39 +0600

    Add files via upload

---
+++ Module 3.5 Practice Day 02/problem_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int N;
+   
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i++) 
+    {
+        printf("I Love Practice\n");
+    }
+    
+    return 0;
+}
---
+++ Module 3/loop_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{  
+    int i;
+    for(i=50;i>=25;i=i-13)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
